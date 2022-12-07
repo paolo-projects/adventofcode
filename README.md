2851244100a48bf36abc46921d31765a27782afd
Paolo Infante <info@paoloinfante.it>
2022-12-07 01:10:30 +0100

    completed day 3

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Solutions for the advent of code puzzle games.
 
 - ~~Day 1~~
 - ~~Day 2~~
-- *Day 3*
+- ~~Day 3~~
 - Day 4
 - Day 5
 - Day 6
--- day_3/src/main/kotlin/Main.kt
+++ day_3/src/main/kotlin/Main.kt
@@ -1,10 +1,8 @@
-
-
-fun mapToPriority(c: Char): Int {
-    return if(c.isLowerCase()) {
-        c.code - 0x61 + 1
-    } else if(c.isUpperCase()) {
-        c.code - 0x41 + 27
+fun Char.mapToPriority(): Int {
+    return if (isLowerCase()) {
+        code - 0x61 + 1
+    } else if (isUpperCase()) {
+        code - 0x41 + 27
     } else {
         throw Exception("Character not in range of lowercase and uppercase characters")
     }
@@ -14,23 +12,43 @@ fun main(args: Array<String>) {
     println("Loading input file...")
 
     val sharedItems = arrayListOf<Char>()
+    val groupsIntersections = arrayListOf<Set<Char>>()
 
-    val input = { }.javaClass.getResourceAsStream("input.txt")?.bufferedReader() ?: throw Exception("input.txt file not found")
+    val input =
+        { }.javaClass.getResourceAsStream("input.txt")?.bufferedReader() ?: throw Exception("input.txt file not found")
+    var i = 0;
 
-    for(line in input.lines()) {
+    for (line in input.lines()) {
         val firstSack = line.take(line.length / 2)
         val lastSack = line.drop(line.length / 2)
 
         val intersection = firstSack.toSet().intersect(lastSack.toSet())
 
-        if(intersection.size == 1) {
+        if (intersection.size == 1) {
             sharedItems.add(intersection.elementAt(0))
         } else {
             throw Exception("More than one shared item in the sacks")
         }
+
+        if (i.mod(3) == 0) {
+            if ((groupsIntersections.lastOrNull()?.size ?: 0) > 1) {
+                throw Exception("More than one badge in last group")
+            }
+
+            groupsIntersections.add(line.toSet())
+        } else {
+            groupsIntersections[groupsIntersections.size - 1] = groupsIntersections.last().intersect(line.toSet())
+        }
+
+        i += 1
     }
 
-    val priorities = sharedItems.fold(0) { acc, char -> acc + mapToPriority(char) }
+    val priorities = sharedItems.fold(0) { acc, char -> acc + char.mapToPriority() }
 
     println("Priorities sum: $priorities")
+
+    val sumOfBadges =
+        groupsIntersections.fold(0) { acc, intersection -> acc + intersection.elementAt(0).mapToPriority() }
+
+    println("Sum of badges: $sumOfBadges")
 }
